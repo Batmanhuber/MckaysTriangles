5f92165efa3051aa1759aff0ae90a5fba0614701
Batmanhuber <mckay.huber@asu.edu>
2021-02-07 11:19:45 -0700

    Saving just in case, 3 text boxes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,13 +26,37 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+        // private void InitializeComponent()
+        // {
+        //     this.textBox1 = new System.Windows.Forms.TextBox();
+        //     this.textBox2 = new System.Windows.Forms.TextBox();
+        //     this.textBox3 = new System.Windows.Forms.TextBox();
+        //     this.SuspendLayout();
+        //     // 
+        //     // textBox1
+        //     // 
+        //     // this.textBox1.AcceptsReturn = true;
+        //     // this.textBox1.AcceptsTab = true;
+        //     this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+        //     //this.textBox1.Multiline = true;
+        //     //this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        //     // 
+        //     // textBox2
+        //     // 
+        //     this.textBox2.Dock = System.Windows.Forms.DockStyle.Left;
+        //     // 
+        //     // textBox3
+        //     // 
+        //     this.textBox3.Dock = System.Windows.Forms.DockStyle.Left;
+        //     // 
+        //     // Form1
+        //     // 
+        //     this.ClientSize = new System.Drawing.Size(284, 264);
+        //     this.Controls.Add(this.textBox1);
+        //     this.Text = "TextBox Example";
+        //     this.ResumeLayout(false);
+        //     this.PerformLayout();
+        // }
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,44 @@ namespace MckaysTriangles
 {
     public partial class Form1 : Form
     {
+        private TextBox textBox1,textBox2,textBox3;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            // this.textBox1.AcceptsReturn = true;
+            // this.textBox1.AcceptsTab = true;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            //this.textBox1.Multiline = true;
+            //this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Text = "TextBox Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
 }
