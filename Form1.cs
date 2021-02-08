6f581b36a17f1d0db85ce9ddfc9afbf0c1757e88
Batmanhuber <mckay.huber@asu.edu>
2021-02-07 21:53:57 -0700

    basic layout done

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,11 @@ namespace MckaysTriangles
 {
     public partial class Form1 : Form
     {
+        private Label label1;
         private TextBox textBox1,textBox2,textBox3;
+        private Button button;
+        private SplitContainer splitContainer;
+        private FlowLayoutPanel flowLayoutPanel;
         public Form1()
         {
             InitializeComponent();
@@ -20,33 +24,59 @@ namespace MckaysTriangles
 
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
+            //
+            // label1
+            //
+            this.label1.Dock = System.Windows.Forms.DockStyle.None;
+            this.label1.Text = "Enter three sides\nfor a triangle";
+            this.label1.Size = new Size (label1.PreferredWidth, label1.PreferredHeight);
             // 
             // textBox1
             // 
-            // this.textBox1.AcceptsReturn = true;
-            // this.textBox1.AcceptsTab = true;
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            //this.textBox1.Multiline = true;
-            //this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             // 
             // textBox2
             // 
-            this.textBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             // 
             // textBox3
-            // 
-            this.textBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            //
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            //
+            // button
+            //
+            this.button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button.Text = "Submit";
+            this.button.Size = new Size (button.PreferredSize.Width,button.PreferredSize.Height);
+            //
+            // flowLayout Panel
+            //
+            this.flowLayoutPanel.FlowDirection = FlowDirection.TopDown;
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel.Controls.Add(label1);
+            this.flowLayoutPanel.Controls.Add(textBox1);
+            this.flowLayoutPanel.Controls.Add(textBox2);
+            this.flowLayoutPanel.Controls.Add(textBox3);
+            this.flowLayoutPanel.Controls.Add(button);
+            //
+            // Split Container
+            //
+            splitContainer.BorderStyle = BorderStyle.Fixed3D;
+            splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer.Panel1.Controls.Add(this.flowLayoutPanel);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(500, 400);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.splitContainer);
             this.Text = "TextBox Example";
             this.ResumeLayout(false);
             this.PerformLayout();
