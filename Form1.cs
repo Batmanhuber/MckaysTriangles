431fe2b099934392ff8f4faa10c552dbf5062ace
Batmanhuber <mckay.huber@asu.edu>
2021-02-07 23:22:25 -0700

    Almost Done, everything but triangle types

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,8 @@ namespace MckaysTriangles
 {
     public partial class Form1 : Form
     {
-        private Label label1;
-        private TextBox textBox1,textBox2,textBox3;
-        private Button button;
+        private Label label1, label2;
+        private NumericUpDown number1, number2, number3;
         private SplitContainer splitContainer;
         private FlowLayoutPanel flowLayoutPanel;
         public Form1()
@@ -25,10 +24,10 @@ namespace MckaysTriangles
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.number1 = new System.Windows.Forms.NumericUpDown();
+            this.number2 = new System.Windows.Forms.NumericUpDown();
+            this.number3 = new System.Windows.Forms.NumericUpDown();
             this.splitContainer = new System.Windows.Forms.SplitContainer();
             this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
@@ -36,50 +35,124 @@ namespace MckaysTriangles
             // label1
             //
             this.label1.Dock = System.Windows.Forms.DockStyle.None;
-            this.label1.Text = "Enter three sides\nfor a triangle";
+            this.label1.Text = "Enter side Lengths \na, b, and c";
             this.label1.Size = new Size (label1.PreferredWidth, label1.PreferredHeight);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            // 
-            // textBox3
             //
-            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            // label2
             //
-            // button
-            //
-            this.button.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button.Text = "Submit";
-            this.button.Size = new Size (button.PreferredSize.Width,button.PreferredSize.Height);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Text = "Please Select Three Side Lengths that make a Valid Triangle";
+            this.label2.Size = new Size (label2.PreferredWidth, label2.PreferredHeight);
+            // 
+            // Number1
+            // 
+            this.number1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.number1.Maximum = 5000;
+            this.number1.ValueChanged += new EventHandler(number1_Change);
+            // 
+            // Number2
+            // 
+            this.number2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.number2.Maximum = 5000;
+            this.number2.ValueChanged += new EventHandler(number2_Change);
+            // 
+            // Number3
+            // 
+            this.number3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.number3.Maximum = 5000;
+            this.number3.ValueChanged += new EventHandler(number3_Change);
             //
             // flowLayout Panel
             //
             this.flowLayoutPanel.FlowDirection = FlowDirection.TopDown;
             this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel.Controls.Add(label1);
-            this.flowLayoutPanel.Controls.Add(textBox1);
-            this.flowLayoutPanel.Controls.Add(textBox2);
-            this.flowLayoutPanel.Controls.Add(textBox3);
-            this.flowLayoutPanel.Controls.Add(button);
+            this.flowLayoutPanel.Controls.Add(number1);
+            this.flowLayoutPanel.Controls.Add(number2);
+            this.flowLayoutPanel.Controls.Add(number3);
             //
             // Split Container
             //
             splitContainer.BorderStyle = BorderStyle.Fixed3D;
             splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             splitContainer.Panel1.Controls.Add(this.flowLayoutPanel);
+            splitContainer.Panel2.Controls.Add(this.label2);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(500, 400);
             this.Controls.Add(this.splitContainer);
-            this.Text = "TextBox Example";
+            this.Text = "Triangle Information";
             this.ResumeLayout(false);
             this.PerformLayout();
+        }
+
+        //
+        // Event handlers for submission of side lengths 1 2 and 3
+        //
+        private void number1_Change(object sender, System.EventArgs e){
+            if(this.valid_input()){
+                this.label2.Text = check_Triagle();
+            }
+            else{
+                this.label2.Text = "Please Select Three Side Lengths that make a Valid Triangle";
+            }
+            
+        }
+        private void number2_Change(object sender, System.EventArgs e){
+            if(this.valid_input()){
+                this.label2.Text = check_Triagle();
+            }
+            else{
+                this.label2.Text = "Please Select Three Side Lengths that make a Valid Triangle";
+            }
+            
+        }
+        private void number3_Change(object sender, System.EventArgs e){
+            if(this.valid_input()){
+                this.label2.Text = check_Triagle();
+            }
+            else{
+                this.label2.Text = "Please Select Three Side Lengths that make a Valid Triangle";
+            }
+            
+        }
+
+        // TODO:
+        // make sure input is a valid triangle
+        private bool valid_input(){
+            bool valid = false;
+            decimal num1 = this.number1.Value;
+            decimal num2 = this.number2.Value;
+            decimal num3 = this.number3.Value;
+
+            if(this.number1.Value == 0 || this.number2.Value == 0 || this.number3.Value == 0){
+                valid = false;
+            }else if(num1 + num2 > num3 && num2 + num3 > num1 && num3 + num1 > num2){
+                valid = true;
+            }
+
+            return valid;
+        }
+
+        // TODO:
+        // check type of tirangle return info
+        private String check_Triagle(){
+            return "Valid Triangle!";
+        }
+
+        //
+        // find max side
+        private decimal my_Max(decimal num1, decimal num2, decimal num3){
+            decimal max = num1;
+
+            if(max < num2){
+                max = num2;
+            }
+            if(max < num3){
+                max = num3;
+            }
+            return max;
         }
     }
 }
